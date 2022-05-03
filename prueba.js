8374493b9884af604eba567a82a313176fcdeda9
JorgeTarancon <57794731+JorgeTarancon@users.noreply.github.com>
2022-05-03 19:01:03 +0200

    Add files via upload

---
+++ prueba.js
@@ -0,0 +1,14 @@
+{
+	"Agno": 2002,
+	"Periodo": "Trimestre 1",
+	"Parametro": "2002",
+	"Valor": 2152.8000243902197,
+	"Estado": "Definitivo"
+},
+{
+	"Agno": 2002,
+	"Periodo": "Trimestre 2",
+	"Parametro": "Trimestre 2",
+	"Valor": 2103.3000137805943,
+	"Estado": "Definitivo"
+}
